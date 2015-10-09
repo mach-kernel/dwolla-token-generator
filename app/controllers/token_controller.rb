53fc72d9a624e9c1e0dd424dd7dc4bdd737910d0
David Stancu <dstancu@nyu.edu>
2015-10-09 16:17:10 -0400

    make application logic more robust, add dwolla styles

--- app/controllers/token_controller.rb
+++ app/controllers/token_controller.rb
@@ -29,7 +29,7 @@ class TokenController < ApplicationController
       :client_id => cred ? dw_env ? Rails.application.secrets.key_prod : Rails.application.secrets.key_uat : params[:auth][:user_key],
       :client_secret => cred ? dw_env ? Rails.application.secrets.sec_prod : Rails.application.secrets.sec_uat : params[:auth][:user_secret],
       :scope => scope[0..scope.length - 2],
-      :response_type => 'code',
+      #:response_type => 'code',
       :redirect_uri => Rails.application.secrets.redirect,
       :dwolla_landing => 'login'
     }
@@ -38,7 +38,7 @@ class TokenController < ApplicationController
   end
 
   def redirect
-    if not (params.key?('code') || session[:req].is_a(Hash))
+    if not (params.key?('code') || session.key?(:req))
       flash[:error] = 'We did not receive any useful data from the Dwolla API. Please try again later.'
       redirect_to '/'
     else
